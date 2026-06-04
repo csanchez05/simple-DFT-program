c4cabfa978b2fc95a7675323859faad9a8e1046a
Calvin Sanchez <calvinsanchez@usf.edu>
2026-06-04 09:07:43 -0400

    Added small test hamiltonian

--- dft_v1.py
+++ dft_v1.py
@@ -1,11 +1,11 @@
 import numpy as np
 
-#physical params
+# Parameters
 a = 10.0     # Lattice Spacing
 E_cut = 20.0 # plane-wave energy cutoff
 Num_k = 10   # num of k-points
 
-#reciprocal lattice vectors
+# Basis Setup 
 m_max = np.sqrt(2 * E_cut) * a / (2 * np.pi) # max G vector
 m_max = int(m_max)
 
@@ -19,12 +19,15 @@ x = np.linspace(0, a, N_grid, endpoint=False) # Real space grid from 0 to a
 # kpoints in the first brillouin zone
 k_points = np.linspace(-np.pi/a, np.pi/a, Num_k, endpoint=False)
 
+
+
+# Defining Potential & Kinetic Energy Functions
 def get_V_ext(x, a, alpha = 1.0):
     dx = np.abs(x - a/2) #distance to center
     dx = np.minimum(dx, a - dx) #applying minimum image convention
     return -1.0 / np.sqrt(dx**2 + alpha)
 
-def get_V_xc(n): #temporary
+def get_V_xc(n): #temporary toy exchange potential
     n_safe = np.maximum(n, 1e-12) #prevent division by zero
     lda_xc = -(3.0 / np.pi * n_safe)**(1/3)
     return lda_xc
@@ -59,13 +62,7 @@ V_ext_x = get_V_ext(x, a)
 V_ext_matrix = get_potential_matrix(V_ext_x, x, G, a)
 #We need to ensure the potential matrix is hermitian. Numerical approx may not be, so we force it.
 V_ext_matrix = 0.5 * (V_ext_matrix + V_ext_matrix.conj().T)
-print("External potential array shape:")
-print(V_ext_x.shape)
-print("External potential matrix shape:")
-print(V_ext_matrix.shape)
-print("External potential matrix:")
-print(V_ext_matrix)
- 
+
 def get_kinetic_matrix(k_point, G):
     N_G = len(G) #Num of plane waves
     T_matrix = np.zeros((N_G, N_G)) #Creates N_G x N_G matrix with all zeros
@@ -76,15 +73,27 @@ def get_kinetic_matrix(k_point, G):
     ## our plane wave basis set (orthogonal))
     return T_matrix
 
+
+
+# Test Hamiltonian
+
 k_test = k_points[0]
+
 T_test = get_kinetic_matrix(k_test, G)
-print("Kinetic matrix shape:")
-print(T_test.shape)
-print("Kinetic matrix:")
-print(T_test)
 
+V_ext_x = get_V_ext(x, a)
+V_ext_matrix = get_potential_matrix(V_ext_x, x, G, a)
 
+V_ext_matrix = 0.5 * (V_ext_matrix + V_ext_matrix.conj().T)
+
+H_test = T_test + V_ext_matrix
+
+eigenvalues, eigenvectors = np.linalg.eigh(H_test)
 
 print("Setup Complete")
 print(f"Number of plane waves: {len(G)}")
 print(f"Real space grid size: {len(x)}")
+print("Hamiltonian shape:")
+print(H_test.shape)
+print("Eigenvalues:")
+print(eigenvalues)
